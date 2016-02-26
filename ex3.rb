8dba71d7355e3a429c8671e4b9627c6d92a10b1a
AppleTim <tim1997@hotmail.fr>
2016-02-26 16:12:16 +0100

    Update ex3.rb

--- ex3.rb
+++ ex3.rb
@@ -1,21 +1,21 @@
 puts "Hens #{25 + 30 / 6}" #Affiche Hens et le résultat 30 divisé par 6 plus 25
 puts "Roosters #{100 - 25 * 3 % 4}"#Affiche Roosters et le résultat du module de 3 par 4 fois 25 que l'on soustrait à 100 
 
-puts "Now I will count the eggs:"
+puts "Now I will count the eggs:"#on compte les oeufs
 
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3 + 2 < 5 - 7?"#est-ce que 3 plus 2 est inférieur à 5 moins 7
 
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7#faux 3 plus 2 n'est pas inférieur à 5 moins 7
 
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3 + 2}"#quel est le résultat de 3 plus 2, on trouve 5
+puts "What is 5 - 7? #{5 - 7}"#quel est le résultat de 5 moins 7, on trouve -2
 
-puts "Oh, that's why it's false."
+puts "Oh, that's why it's false."#on comprend pourquoi on a trouvé faux
 
 puts "How about some more."
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it greater? #{5 > -2}"#est-ce que 5 est suppérieur à -2, on trouve que oui
+puts "Is it greater or equal? #{5 >= -2}"#est-ce que 5 est suppérieur ou égal à -2, on trouve que oui
+puts "Is it less or equal? #{5 <= -2}"#est-ce que 5 est inférieur ou égal à -2, on trouve que non
