ffdff45d3e761c22f0a3cff529f4741481b16b09
AppleTim <tim1997@hotmail.fr>
2016-02-26 15:58:17 +0100

    Create ex3.rb

---
+++ ex3.rb
@@ -0,0 +1,21 @@
+puts "Hens #{25 + 30 / 6}" #Affiche Hens et le résultat 30 divisé par 6 plus 25
+puts "Roosters #{100 - 25 * 3 % 4}"#Affiche Roosters et le résultat du module de 3 par 4 fois 25 que l'on soustrait à 100 
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
